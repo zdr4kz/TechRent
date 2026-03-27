3822766ae6011ac0957f43099e84f4093c8bcf7f
Arian <arianmonteiromendes@gmail.com>
2026-03-27 09:57:33 -0300

    FIX: corrigir verificação de email existente e melhorar o fluxo de registro de usuário

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -16,53 +16,68 @@ const { create, read } = require('../config/database.js')
 
 // POST /auth/registro - cria um novo usuário
 const registro = async (req, res) => {
-  try{
+  try {
     const { nome, email, senha } = req.body
 
 
     //  VERIFICA SE TEM UM EMAIL EXISTENTE
     const user = await read("usuarios")
-    if(user[0].email == email){
+    const userExistente = user.find(u => u.email === email)
+
+    console.log()
+
+
+    if (userExistente) {
       return res.status(400).json({
         sucesso: false,
         mensagem: "email já cadastrado!"
       })
     }
-    
-    // CRIPTOGRAFA A SENHA
-    const salt = await bcrypt.genSalt(10);
-    const senhaCriptografada = await bcrypt.hash(senha, salt)
-    
 
-    // CRIA O ARQUIVO DATA COM A SENHA JÁ CRIPTOGRAFA E COM NIVEL DE ACESSO CLIENTE
-    const data = {
-      nome: nome,
-      email: email,
-      senha: senhaCriptografada,
-      nivel_acesso: "cliente"
-    }
+      // CRIPTOGRAFA A SENHA
+      const salt = await bcrypt.genSalt(10);
+      const senhaCriptografada = await bcrypt.hash(senha, salt)
 
 
-    // CRIA O USUÁRIO NO BANCO 
-    await create("usuarios", data)
-    res.status(200).json({
+      // CRIA O ARQUIVO DATA COM A SENHA JÁ CRIPTOGRAFA E COM NIVEL DE ACESSO CLIENTE
+      const data = {
+        nome: nome,
+        email: email,
+        senha: senhaCriptografada,
+        nivel_acesso: "cliente"
+      }
+
+
+
+      // CRIA O USUÁRIO NO BANCO 
+      const id = await create("usuarios", data)
+
+      const payload = {
+        id: id,
+        nome: nome,
+        email: email,
+        nivel_acesso: "cliente"
+
+      } 
+
+      res.status(200).json({
         sucesso: true,
         mensagem: "você foi registrado com sucesso!"
-    })
+      })
 
-  } 
-  catch(e) {
+  }
+  catch (e) {
     res.status(500).json({
       sucesso: false,
       mensagem: "erro no sistema",
       erro: e
     })
-  } 
-  
+  }
+
 
 
   // TODO
-  
+
 };
 
 // POST /auth/login - autentica e retorna JWT
